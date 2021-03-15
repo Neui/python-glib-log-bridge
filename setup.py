d4db54ff290cfde8e6a01c33ff1597888ef66d72
Neui <neuisen@googlemail.com>
2021-03-15 04:12:37 +0100

    Use tests test_suite since it doesnt throw some weird glib renum register error

--- setup.py
+++ setup.py
@@ -3,12 +3,6 @@ import os.path
 import unittest
 
 
-def get_test_suite():
-    test_loader = unittest.TestLoader()
-    test_suite = test_loader.discover('tests', pattern='test_*.py')
-    return test_suite
-
-
 this_directory = os.path.abspath(os.path.dirname(__file__))
 readme_filename = os.path.join(this_directory, 'README.md')
 with open(readme_filename, "r") as f:
@@ -31,5 +25,5 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.5',
-    test_suite="setup.get_test_suite",
+    test_suite="tests",
 )
